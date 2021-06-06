9300c5f3b0f00ebd36bc9cfc6ab3379760607202
RJUDODA <78902484+RJUDODA@users.noreply.github.com>
2021-06-06 16:21:49 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,16 +1,83 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+#ifndef INCLUDE_TPQUEUEH
+#define INCLUDE_TPQUEUEH
 #include <cassert>
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+ private:
+    T* arr;
+    int size;
+    int begin, end;
+    int count;
+    int stepBack(int index) {
+        int res = --index;
+        if (res < 0)
+            res += size + 1;
+        return res;
+    }
+    int stepForward(int index) {
+        int res = ++index;
+        if (res > size)
+            res -= size + 1;
+        return res;
+    }
+
+ public:
+    TPQueue() :
+        size(100),
+        begin(0), end(0), count(0) {
+        arr = new T[size + 1];
+    }
+    ~TPQueue() {
+        delete[] arr;
+    }
+
+    void push(const T& item) {
+        assert(count < size);
+        if (isEmpty()) {
+            arr[end] = item;
+        } else {
+            int current = stepBack(end);
+            if (arr[current].prior >= item.prior) {
+                arr[end] = item;
+            } else {
+                do {
+                    arr[stepForward(current)] = arr[current];
+                    current = stepBack(current);
+                } while (arr[current].prior < item.prior &&
+                    stepForward(current) != begin);
+                arr[stepForward(current)] = item;
+            }
+        }
+        end = stepForward(end);
+        count++;
+    }
+
+    T pop() {
+        assert(count > 0);
+        T item = arr[begin];
+        count--;
+        begin = stepForward(begin);
+        return item;
+    }
+
+    T get() const {
+        assert(count > 0);
+        return arr[begin];
+    }
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+
+    bool isFull() const {
+        return count == size;
+    }
 };
 
 struct SYM {
   char ch;
   int  prior;
 };
-
-#endif // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUEH
